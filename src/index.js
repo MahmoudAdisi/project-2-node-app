077e5228efa868fc63e9e3197ba2f81c7083dbdf
mamoud.adisi <mahmoud.aam@outlook.com>
2025-02-17 23:19:38 +0200

    update

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ app.get('/', (req, res) => {
             <li>Docker for running all services efficiently</li>
         </ul>
         <p>The application is now running on <strong>http://localhost:4000</strong></p>
-        <h1>Regards, Mahmoud Adisi...build</h1>
+        <h1>Regards, Mahmoud Adisi. build</h1>
         <p>Mahmoud.aam@outlook.com</p>
 
     `);
